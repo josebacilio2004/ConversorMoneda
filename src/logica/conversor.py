5dd4c3c0ceda2beb39cfcaa5ca10d287e77b6f0c
LAB_F402 <labf402@continental.edu.pe>
2024-11-04 19:25:09 -0500

    actualización worskpace.xml

--- src/logica/conversor.py
+++ src/logica/conversor.py
@@ -5,7 +5,13 @@ class ConversorMoneda:
         ('EUR', 'JPY'): 130,
         ('EUR', 'USD'): 1 / 0.85,
         ('JPY', 'USD'): 1 / 110,
-        ('JPY', 'EUR'): 1 / 130
+        ('JPY', 'EUR'): 1 / 130,
+        ('PEN', 'USD'): 0.27,
+        ('PEN', 'EUR'): 0.24,
+        ('PEN', 'JPY'): 40.38,
+        ('USD', 'PEN'): 1/0.27,
+        ('EUR', 'PEN'): 1/0.24,
+        ('JPY', 'PEN'): 1/40.38
     }
 
     def convertir(self, monto, moneda_origen, moneda_destino):
